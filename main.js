f1ecc34c63929ae445219f57dc5df22a1ff539f3
Anel Askarkyzy <104404613+Askarkyzy@users.noreply.github.com>
2022-12-19 10:05:06 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 const header = document.querySelector("header");
 
+
+
 window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 0);
 });
+
+
+
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+}
